fe7f0cbc0502ef4b466919690f2edc573422c433
Brian W Bush <code@functionally.io>
2020-11-25 13:40:25 -0700

    added tarot and tidied html

--- controller.js
+++ controller.js
@@ -1,12 +1,19 @@
 
-function showOverlay(name) {
+function showArt(name) {
   overlay.style.display = "flex"
   overlay_image.src = "art/" + name + ".jpg"
   overlay_text.innerHTML = document.getElementById(name).innerHTML
 }
 
+function showImage(image) {
+  overlay.style.display = "flex"
+  overlay_image.src = image.src
+  overlay_text.innerText = image.alt.slice(0, -4)
+}
+
 function hideOverlay() {
   overlay.style.display = "none"
+  main.focus()
 }
 
 function setup() {
